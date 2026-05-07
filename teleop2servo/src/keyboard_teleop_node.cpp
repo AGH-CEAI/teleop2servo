236c64305e83a3a5b2b5e3aea9e5d2bbcffbefd7
AntoniRL <aradominski@agh.edu.pl>
2026-05-07 10:11:45 +0200

    repair cont movement

--- teleop2servo/src/keyboard_teleop_node.cpp
+++ teleop2servo/src/keyboard_teleop_node.cpp
@@ -203,6 +203,8 @@ void KeyboardTeleopNode::stop_motion(const std::string &reason)
   continuous_repeat_seen_ = false;
   active_cmd_ = ActiveCmd{};
   have_active_cmd_ = true; // once with ActiveCmdType::NONE to stop motion.
+  
+  if (speed_mode_ != SpeedMode::STEP) active_char_ = 0;
 }
 
 void KeyboardTeleopNode::switch_control_mode()
